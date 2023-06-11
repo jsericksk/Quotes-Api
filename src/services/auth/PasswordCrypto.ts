5b044b3ee84376261e948b1b0a988c25a6ae00b2
jsericksk <jsericksk@gmail.com>
2023-06-10 21:18:11 -0300

    refactor: Pequene melhoria no retorno das funções da PasswordCrypto

--- src/services/auth/PasswordCrypto.ts
+++ src/services/auth/PasswordCrypto.ts
@@ -3,12 +3,12 @@ import bcrypt from "bcryptjs";
 export class PasswordCrypto {
     private SALT_ROUNDS = 10;
 
-    async hashPassword(password: string) {
+    async hashPassword(password: string): Promise<string> {
         const saltGenerated = await bcrypt.genSalt(this.SALT_ROUNDS);
         return await bcrypt.hash(password, saltGenerated);
     }
 
-    async verifyPassword(password: string, hashedPassword: string) {
+    async verifyPassword(password: string, hashedPassword: string): Promise<boolean> {
         return await bcrypt.compare(password, hashedPassword);
     }
 }
