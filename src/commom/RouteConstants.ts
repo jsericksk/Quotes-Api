338618220e4e3d0dbacb77373780c96d8278a9f5
jsericksk <jsericksk@gmail.com>
2023-06-12 04:59:22 -0300

    feat: Adicionadas rotas de quotes na RouteConstants

--- src/commom/RouteConstants.ts
+++ src/commom/RouteConstants.ts
@@ -2,3 +2,12 @@ export const AuthRoute = {
     register: "/auth/register",
     login: "/auth/login",
 };
+
+export const QuoteRoute = {
+    create: "/quotes",
+    getAll: "/quotes",
+    getAllFromUser: "/quotes/user/:userId",
+    getById: "/quotes/:id",
+    update: "/quotes/:id",
+    delete: "/quotes/:id",
+};
