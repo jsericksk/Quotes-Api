2879742f0bda46dd3ffde3aca2dfcf850d652785
jsericksk <jsericksk@gmail.com>
2023-06-21 12:08:42 -0300

    fix: Correção no seed de users para evitar incluir id

--- src/database/seeds/0001_insert_users.ts
+++ src/database/seeds/0001_insert_users.ts
@@ -8,21 +8,18 @@ export async function seed(knex: Knex) {
     await knex(Table.users).insert(users);
 }
 
-const users: User[] = [
+const users: Omit<User, "id">[] = [
     {
-        id: 1,
         email: "test_user1@gmail.com",
         username: "John - Test",
         password: "$2a$10$bLBU0djn2psZCYnEZCA0NO1m7GtWMK6.KNnCJuLT6W7tuXcE9DD/a"
     },
     {
-        id: 2,
         email: "test_user2@gmail.com",
         username: "Anna - Test",
         password: "$2a$10$fhUtPKpqmlDqTF/KEGvoH.vShwVZIr0N9ll2CcbR54Pwskzj2m5Du"
     },
     {
-        id: 3,
         email: "test_user3@gmail.com",
         username: "Mary - Test",
         password: "$2a$10$S2i3RTQBQn0ti3RX.phoueL.bRKtlS5RT1cIIH4mV6./R9pm5umqe"
