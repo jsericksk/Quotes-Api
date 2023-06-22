b0ccdbe6aefca22e4cd3a05bbcf34e791d8c5bea
jsericksk <jsericksk@gmail.com>
2023-06-22 00:20:00 -0300

    feat: Criada interface QuotesRepository

---
+++ src/repositories/quotes/QuotesRepository.ts
@@ -0,0 +1,10 @@
+import { Quote } from "../../models/Quote";
+
+export interface QuotesRepository {
+    getAll(page: number, limit: number, filter: string): Promise<Quote[]>
+    getById(id: number): Promise<Quote | null>
+    create(quote: Omit<Quote, "id">): Promise<number>
+    updateById(quoteId: number, loggedInUserId: number, quote: Omit<Quote, "id">): Promise<void>
+    deleteById(quoteId: number, loggedInUserId: number): Promise<void>
+    count(filter: string): Promise<number>
+}
