f7a7f7c9bab45f86aeb2d162f976c131a3d4d1ae
jsericksk <jsericksk@gmail.com>
2023-07-02 15:26:06 -0300

    refactor: Pequena alteração na simpleError para sempre retornar um error_code padrão

--- src/errors/simpleError.ts
+++ src/errors/simpleError.ts
@@ -1,6 +1,3 @@
-export const simpleError = (message: string, errorCode = "") => {
-    if (errorCode.length > 0) {
-        return { error: message, error_code: errorCode };
-    }
-    return { error: message };
+export const simpleError = (message: string, errorCode = "unspecified") => {
+    return { error: message, error_code: errorCode };
 };
